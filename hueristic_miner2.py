c0583cd7b24dcd714cd0317c7ea230fe446ab6a1
michaliszyn94 <benjaminmichaliszyn@gmail.com>
2025-03-18 20:12:24 -0500

    finished saving the node paths used for proof for each access request

--- hueristic_miner2.py
+++ hueristic_miner2.py
@@ -42,7 +42,7 @@ def find_connectivity(node: int, depth: int, graph: nx.Graph) -> int:
 def complete_policy(policy:dict):
     return all(value is not None for value in policy.values())
 
-def examine(node: int, graph: nx.Graph, neighbors: set, lla: dict, policy: dict, n: int, cd: set, cg :set, proof: set, r_pairs: set):
+def examine(node: int, graph: nx.Graph, neighbors: set, lla: dict, policy: dict, n: int, cd: set, cg :set, proof: set, r_pairs: set, node_paths: dict):
     grant_dict = {}
     targets = [neighbor for neighbor in neighbors if lla.get(node, {}).get(neighbor) is True]
     grants, denies = find_viable_paths(graph, node, targets, n) 
@@ -53,11 +53,12 @@ def examine(node: int, graph: nx.Graph, neighbors: set, lla: dict, policy: dict,
     # In addition, anytime we find something out about the policy, we must look at the unsolved node pairs
   
     for d in denies:
-        pattern, visited, target_node = d[0], d[1], d[2]
+        pattern, visited, target_node, node_path = d[0], d[1], d[2], d[3]
         if pattern not in cd and lla.get(node, {}).get(target_node) == False: #### I SPENT 9 HOURS FIGURING OUT TO ADD THE AND STATEMENT
             # I'm assuming if we do not have a record of something granting access, it will default to a deny, have to avoid this!
             cd.add(pattern)
             policy[pattern] = False
+            node_paths[pattern] = node_path
             check_existing_grants = True
             proof |= visited
             r_pairs.add((node, target_node))
@@ -66,34 +67,32 @@ def examine(node: int, graph: nx.Graph, neighbors: set, lla: dict, policy: dict,
     # Each potential grant path is filtered out by confirmed denies. 
     # The remaining ones are added to a dictionary with key as a tuple (source, target), value is a tuple (path, visited)
     for g in grants:                                                                    
-        target_node, path, visited = g[0], g[1], g[2]
+        target_node, path, visited, node_path = g[0], g[1], g[2], g[3]
         proof |= visited
         r_pairs.add((node, target_node))
         if path in cd:
             continue      
-        grant_dict.setdefault((node, target_node), []).append((path, visited))
+        grant_dict.setdefault((node, target_node), []).append((path, visited, node_path))
         
-        # This line is actually so huge. Even if one iteration doesn't use an 
-        # proof |= visited
-        # r_pairs.add((node, target_node))
+        
     
     for key, paths in grant_dict.items():
         if len(paths) == 1:
-            path, visited = paths[0]  
-        # if path not in cg:
+            path, visited, node_path = paths[0]  
             cg.add(path)
             policy[path] = True
+            node_paths[pattern] = node_path
             proof |= visited  
             r_pairs.add((node, key[1]))
         else:
-            add_to_npg.extend((key, p_v) for p_v in paths)
+            add_to_npg.extend((key, p_v, node_path) for p_v in paths)
             
     return check_existing_grants, add_to_npg
    
 def find_viable_paths(graph: nx.Graph, node: int, targets:List[int], max_length: int):
     g_paths, d_paths= [], [] #next_node, new_path, new_visited for g_paths | new,path, new_visited, for d_paths
     
-    def seek(node: int, depth: int, path: str, visited: set):
+    def seek(node: int, depth: int, path: str, visited: set, node_path: List[int]):
         #Base Case
         if depth >= max_length:
             return
@@ -109,35 +108,38 @@ def find_viable_paths(graph: nx.Graph, node: int, targets:List[int], max_length:
                     # Iterate through all the types
                     for c in type_string:
                         new_path = path + c
+                        new_node_path = node_path + [next_node]
                         # If the original node is granted access we add to the g_path else, to the d_paths
                         if next_node in targets:
-                            g_paths.append((next_node, new_path, new_visited))
+                            g_paths.append((next_node, new_path, new_visited, new_node_path))
                         else :                                                     ### I dream of getting rid of all irrelavent paths ###
-                            d_paths.append((new_path, new_visited, next_node))
-                        seek(next_node, depth + 1, new_path, new_visited)
+                            d_paths.append((new_path, new_visited, next_node, new_node_path))
+                        seek(next_node, depth + 1, new_path, new_visited, new_node_path)
                             # if lla[node][next_node]:                                                       
                             #     d_paths.append((new_path, new_visited, next_node))
                             #     seek(next_node, depth + 1, new_path, new_visited, lla)
                         
-    seek(node, 0, '', {node})
+    seek(node, 0, '', {node}, [node])
 
     return g_paths, d_paths
 
-def grant_check(policy: dict, cg: set, npg: dict, cd: set, proof: set, r_pairs: set): # need to add proof here as well
+def grant_check(policy: dict, cg: set, npg: dict, cd: set, proof: set, r_pairs: set, node_paths: dict): # need to add proof here as well
     # Create a list of keys to delete after iteration
     to_delete = []
     
     for np in list(npg.keys()):  # Iterate over a copy to modify safely
-         # (source, target), (pattern, visited)
+        # (source, target), (pattern, visited)
         # Remove grants_paths that are in 'cd'
         npg[np] = [grant_path for grant_path in npg[np] if grant_path[0] not in cd] ###
        
         # If only one grant remains, we can add it to confirmed grants
         if len(npg[np]) == 1:  
-            grant = npg[np][0][0]
+            pattern = npg[np][0][0]
             visited = npg[np][0][1]
-            cg.add(grant)  
-            policy[grant] = True  
+            node_path = npg[np][0][2]
+            cg.add(pattern)  
+            policy[pattern] = True
+            node_paths[pattern] = node_path ###
             proof |= visited
             r_pairs.add((np[0], np[1]))
             to_delete.append(np)
@@ -158,13 +160,14 @@ def count_access(data: dict) -> int:
         for sub_dict in data.values() 
         for value in sub_dict.values()
     )
-
+               
 def hueristic_miner(lla: dict, depth: int, r_types: List[str], graph: nx.Graph, max_iterations: int):
     #####
     start = time.time()
     #####
     confirmed_denies, confirmed_grants = set(), set()
     policy = create_policy(depth, r_types)
+    policy_node_paths = {}
     node_pair_grants = {}
     nodes_checked = 0
     iterations = 0
@@ -186,9 +189,9 @@ def hueristic_miner(lla: dict, depth: int, r_types: List[str], graph: nx.Graph, 
             
             # If we find new denies, we must check the existing node-pair grants to see if we can determine a True grant.
             # Each time we examine a node, any node-pair grants that were discovered but not confirmed to be True will be stored
-            check_existing_grants, add_to_npg = examine(node, graph, neighbors, lla, policy, depth, confirmed_denies, confirmed_grants, proof, relevant_pairs)
+            check_existing_grants, add_to_npg = examine(node, graph, neighbors, lla, policy, depth, confirmed_denies, confirmed_grants, proof, relevant_pairs, policy_node_paths)
             if check_existing_grants:
-                grant_check(policy, confirmed_grants, node_pair_grants, confirmed_denies, proof, relevant_pairs)
+                grant_check(policy, confirmed_grants, node_pair_grants, confirmed_denies, proof, relevant_pairs, policy_node_paths)
                 
             # Add the new node pair grants
             for npg in add_to_npg:
@@ -200,35 +203,16 @@ def hueristic_miner(lla: dict, depth: int, r_types: List[str], graph: nx.Graph, 
         iterations +=1
     end = time.time()
     elapsed_time = end - start
+    
+    
     denies = sum(1 for p in policy if policy[p] is False)
     grants = sum(1 for p in policy if policy[p] is True)
     ambiguous = sum(1 for p in policy if policy[p] is None)
     
     # Metadata for print statements
-
+   
     reduced_lla = {}
     
-                #Distance LLA
-    #Let's try removing lla based on distance from nodes
-    # reduced_lla = {}
-    # for node, _, neighbors in sorted_node_list:
-    #     reduced_lla[node] = {}
-    # # Ensure that lla[node] exists and that 'n' exists in lla[node]
-    # for n in neighbors:
-    #     if n in lla.get(node, {}):  # check if the target 'n' exists in lla[node]
-    #         reduced_lla[node].setdefault(n, lla[node][n])
-    # reduced_lla = {}
-    
-                #Large Reduced LLA
-    # #Creating a reduced lla for testing policies, this contains the lla for all the nodes that are used in the search
-    # for i in rpp:
-    #     reduced_lla[i] = {}
-    #     for j in rpp:
-    #         # if i in lla and j in lla[i]:
-    #             reduced_lla[i][j] = lla[i][j]
-                
-                #Best case scenario
-    # I thought this would work, but it doesn't. Essentially, whatever node pairs (1,3) that are being used to prove a rule are being passed down
     for rp in relevant_pairs:
         source, target = rp[0], rp[1]
         if source not in reduced_lla:
@@ -248,5 +232,6 @@ def hueristic_miner(lla: dict, depth: int, r_types: List[str], graph: nx.Graph, 
     print(f'There are {len(relevant_pairs)} relavent pairs')
     print(f'The original lla contains: {(count_access(lla))} access requests')
     print(f'The reduced lla  contains: {(count_access(reduced_lla))} access requests')
+    print(f'The paths for each access request are: {policy_node_paths}')
     
     return elapsed_time
